f4faed4a073c09331393c638a43c46ca2cbd5435
Isola Pelumi <69679124+philtechphilips@users.noreply.github.com>
2024-03-05 16:31:50 +0100

    feat: footer completed

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,61 @@
+<footer class="w-full bg-[#FFF] border-t-2 border-[#E5E5E5] pt-5">
+    <div class="w-full py-10 flex md:flex-row flex-col items-start justify-between gap-8 md:px-20 px-5">
+        <div class="flex flex-col gap-4 md:w-[304px] w-full">
+            <img class="w-32" src="{{ asset('assets/logo.png') }}">
+            <p class="text-[#434343] w-full">
+                Maryson Energy Limited is an indigenous integrated energy and engineering solutions company in Nigeria,
+                delivering value-adding services across the energy value chain.
+            </p>
+        </div>
+
+        <div class="flex flex-col md:w-[153px] w-full">
+            <h1 class="text-[#1D1D1D] font-bold text-xl mb-4">QUICK LINKS</h1>
+            <ul class="flex flex-col gap-4">
+                <li>
+                    <a href="#" class="text-[#434343]">About</a>
+                </li>
+
+                <li>
+                    <a href="#" class="text-[#434343]">Services</a>
+                </li>
+
+                <li>
+                    <a href="#" class="text-[#434343]">Projects</a>
+                </li>
+
+                <li>
+                    <a href="#" class="text-[#434343]">Our Team</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="flex flex-col md:w-36 w-full">
+            <h1 class="text-[#1D1D1D] font-bold text-xl mb-4">ADDRESS</h1>
+            <p class="text-[#434343]">
+                Suite 207, AHCN
+                Towers, 26, CIPM Avenue, CBD, Alausa, Ikeja, Lagos
+            </p>
+        </div>
+
+        <div class="flex flex-col gap-4 md:w-fit w-full">
+            <h1 class="text-[#1D1D1D] font-bold text-xl">CONTACTS</h1>
+            <div class="flex gap-2 items-center">
+                <img src="{{ asset("assets/fluent_call-24-regular.svg")}}"></img>
+                <p class="text-[#434343]">+234 (0) 810 145 5810</p>
+            </div>
+
+            <div class="flex gap-2 items-center">
+                <img src="{{ asset("assets/mdi-light_email.svg")}}"></img>
+                <p class="text-[#434343]">info@maryson-ng.com</p>
+            </div>
+
+            <div class="flex gap-2 items-center">
+                <img src="{{ asset("assets/basil_linkedin-outline.svg")}}"></img>
+                <p class="text-[#434343]">www.maryson-ng.com</p>
+            </div>
+        </div>
+    </div>
+    <div class="w-full text-center bg-[#F2F2F2] py-5">
+        <p class="text-sm text-[#8E8E8E]">Copyright 2023 Maryson Energy.All right reserved</p>
+    </div>
+</footer>
--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,5 +1,29 @@
 <header class="w-full bg-white py-3 px-20">
 <nav class="w-full flex items-center justify-between">
-    <img src="{{ asset("assets/logo.png")}}" class="w-44"></img>
+    <img src="{{ asset("assets/logo.png")}}" class="w-40"></img>
+
+    <ul class="flex gap-10 items-center">
+        <li>
+            <a href="/" class="uppercase underline text-sm text-[#434343] leading-6 py-2"> Home</a>
+        </li>
+
+        <li>
+            <a href="/about-us" class="uppercase underline text-sm text-[#434343] leading-6 py-2"> About</a>
+        </li>
+
+        <li>
+            <a href="/services" class="uppercase underline text-sm text-[#434343] leading-6 py-2"> Services</a>
+        </li>
+
+        <li>
+            <a href="/projects" class="uppercase underline text-sm text-[#434343] leading-6 py-2"> Projects </a>
+        </li>
+
+        <li>
+            <a href="/teams" class="uppercase underline text-sm text-[#434343] leading-6 py-2"> Our Team</a>
+        </li>
+    </ul>
+
+    <img class="w-8 cursor-pointer" src="{{ asset("assets/streamline_interface-setting-menu-parallel-hamburger-circle-navigation-parallel-hamburger-buttonmenu-circle.png")}}">
 </nav>
 </header>
