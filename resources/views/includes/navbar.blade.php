414558f8740023c4e2894fd3a478d2179ec824fb
Isola Pelumi <69679124+philtechphilips@users.noreply.github.com>
2024-03-07 23:01:31 +0100

    feat: mobile navigation completed

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,4 +1,5 @@
-<header class="w-full bg-white py-3 md:px-20 px-5">
+{{-- Desktop navigation --}}
+<header class="w-full bg-white py-3 md:px-20 px-5 header-shadow z-[1000] mb-[2px]">
     <nav class="w-full flex items-center justify-between">
         <img src="{{ asset('assets/logo.png') }}" class="w-40"></img>
 
@@ -29,14 +30,61 @@
             </li>
         </ul>
 
-        <img class="w-8 cursor-pointer toggleButton"
+        <img class="w-8 cursor-pointer toggleButton md:flex hidden"
             src="{{ asset('assets/streamline_interface-setting-menu-parallel-hamburger-circle-navigation-parallel-hamburger-buttonmenu-circle.png') }}">
-    </nav>
+            <i class="ri-menu-2-line md:hidden flex text-2xl menuButton"></i>
+        </nav>
 </header>
+{{-- Desktop navigation --}}
 
+<nav id="menu" class="w-full p-5 md:hidden bg-gray-100 h-screen fixed top-0 translate-x-[500px] duration-[300ms] ease-in transform">
+    <div class="flex items-center justify-between mb-20">
+        <img src="{{ asset('assets/logo.png') }}" class="w-40"></img>
+        
+        <div class="w-10 h-10 menuButton border-[#000] flex items-center justify-center rounded-full border-2 solid">
+            <i class="ri-close-line text-3xl"></i>
+        </div>
+    </div>
+    
+    <ul class="flex flex-col gap-10 items-center list">
+        <li>
+            <a href="/" class="uppercase font-medium text-xl text-[#434343] leading-6 py-2"> Home</a>
+            <div class="w-full h-[1px] bg-[#434343]"></div>
+        </li>
+
+        <li>
+            <a href="/about-us" class="uppercase font-medium text-xl text-[#434343] leading-6 py-2"> About</a>
+            <div class="w-full h-[1px] bg-[#434343]"></div>
+        </li>
+
+        <li>
+            <a href="/services" class="uppercase font-medium text-xl text-[#434343] leading-6 py-2"> Services</a>
+            <div class="w-full h-[1px] bg-[#434343]"></div>
+        </li>
+
+        <li>
+            <a href="/projects" class="uppercase font-medium text-xl text-[#434343] leading-6 py-2"> Projects </a>
+            <div class="w-full h-[1px] bg-[#434343]"></div>
+        </li>
+
+        <li>
+            <a href="/teams" class="uppercase font-medium text-xl text-[#434343] leading-6 py-2"> Our Team</a>
+            <div class="w-full h-[1px] bg-[#434343]"></div>
+        </li>
+
+        <li class="flex gap-2 items-center bg-[#e5e5fd] rounded-lg px-6 py-3">
+                <img src="{{ asset('assets/fluent_call-24-regular.svg') }}"></img>
+                <p class="text-[#434343] font-medium md:text-xl">+234 (0) 810 145 5810</p>
+        </li>
+    </ul>
+
+    </nav>
+
+{{-- Mobile Sidebar --}}
 
 <div id="hiddenDiv" class="w-full hidden toggleButton fixed top-0 bg-black bg-opacity-50 z-50 h-screen left-0"></div>
-<div id="sideBar" class="w-full md:w-[406px] bg-white z-[1000] fixed top-0 rounded-3xl right-10 p-10 h-screen  translate-x-[500px] duration-[300ms] ease-in transform">
+<div id="sideBar"
+    class="w-full md:w-[406px] bg-white z-[1000] fixed top-0 rounded-3xl right-10 p-10 h-screen  translate-x-[500px] duration-[300ms] ease-in transform">
     <div class="cursor-pointer pb-10 flex justify-end">
         <img src=" {{ asset('assets/streamline_interface-setting-menu-parallel-hamburger-circle-navigation-parallel-hamburger-buttonmenu-circle.svg') }}"
             class="w-8 toggleButton">
@@ -80,3 +128,5 @@
         </div>
     </div>
 </div>
+
+{{-- Mobile Sidebar --}}
